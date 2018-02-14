e75f9ef3986f99c95298302c86b08bbe373adc50
IVIachine <tyler.chermely@mymail.champlain.edu>
2018-02-14 10:47:03 -0500

    Added new shader

---
+++ Senses Game/Assets/Scripts/SenseSwitcher.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenseSwitcher : MonoBehaviour {
+
+    [SerializeField]
+    private Material mSwitchMat;
+
+    private List<GameObject> mObjectRefs;
+    private List<Material> mOriginalMats;
+
+    private bool mIsSight;
+    private Material mActualMat, mOldSkybox;
+
+	// Use this for initialization
+	void Start () {
+        mIsSight = true;
+        mActualMat = Material.Instantiate(mSwitchMat);
+        mOldSkybox = RenderSettings.skybox;
+
+        mObjectRefs = new List<GameObject>();
+        mOriginalMats = new List<Material>();
+        MeshRenderer[] renderers = GameObject.FindObjectsOfType<MeshRenderer>();
+
+        foreach(MeshRenderer rend in renderers)
+        {
+            mObjectRefs.Add(rend.gameObject);
+            mOriginalMats.Add(rend.material);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            mIsSight = !mIsSight;
+
+            if(mIsSight)
+            {
+                int index = 0;
+                foreach(GameObject obj in mObjectRefs)
+                {
+                    obj.GetComponent<MeshRenderer>().material = mOriginalMats[index];
+                    index++;
+                }
+
+                RenderSettings.skybox = mOldSkybox;
+            }
+            else
+            {
+                foreach (GameObject obj in mObjectRefs)
+                {
+                    obj.GetComponent<MeshRenderer>().material = mActualMat;
+                }
+
+                RenderSettings.skybox = null;
+            }
+        }
+	}
+}
---
+++ Senses Game/Assets/Shaders/HearingShader.shader
@@ -0,0 +1,59 @@
+﻿Shader "Custom/HearingShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+	}
+
+	SubShader {
+		Blend SrcAlpha OneMinusSrcAlpha
+		Tags { "RenderType"="Transparent" }
+		LOD 200
+
+		Pass
+	{
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma vertex vert
+		#pragma fragment frag
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+
+		struct appdata
+		{
+			float4 vertex : POSITION;
+			float2 uv : TEXCOORD0;
+			float4 oldPos : TEXCOORD1;
+		};
+
+		struct v2f
+		{
+			float2 uv : TEXCOORD0;
+			float4 vertex : SV_POSITION;
+			float4 oldPos : TEXCOORD1;
+		};
+
+		fixed4 _Color;
+
+		v2f vert(appdata v)
+		{
+			v2f o;
+			o.oldPos = v.vertex;
+			o.vertex = UnityObjectToClipPos(v.vertex);
+			o.uv = v.uv;
+			return o;
+		}
+
+		fixed4 frag(v2f IN) : SV_Target
+		{
+			fixed4 col;
+			col = float4(0, 0, 0, 1);
+			return col;
+		}
+		ENDCG
+		}
+	}
+	FallBack "Diffuse"
+}
