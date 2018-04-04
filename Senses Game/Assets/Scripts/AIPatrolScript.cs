368724e950978b8efc2d96a068616970b2b36c29
Tyler Chermely <tyler.chermely@mymail.champlain.edu>
2018-04-04 10:46:22 -0400

    Added AI and stuff

--- Senses Game/Assets/Scripts/AIPatrolScript.cs
+++ Senses Game/Assets/Scripts/AIPatrolScript.cs
@@ -2,29 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 using UnityStandardAssets.Characters.ThirdPerson;
 
-public class AIPatrolScript : MonoBehaviour {
+public class AIPatrolScript : MonoBehaviour
+{
 
     [SerializeField]
     List<Vector3> mPatrolLocations;
 
     [SerializeField]
-    private float mWaitTime;
+    private float mWaitTime, mSeeDistance;
 
     private float mCurrentWait;
     private int mCurrentIndex;
     private bool mPathfinding;
-	// Use this for initialization
-	void Start () {
+    private GameObject mPlayer;
+    // Use this for initialization
+    void Start()
+    {
         mCurrentIndex = 0;
         mCurrentWait = 0.0f;
         mPathfinding = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(!mPathfinding && mCurrentWait <= 0.0f)
+
+        mPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!mPathfinding && mCurrentWait <= 0.0f)
         {
             mCurrentIndex++;
             if (mCurrentIndex >= mPatrolLocations.Count)
@@ -42,7 +49,7 @@ public class AIPatrolScript : MonoBehaviour {
 
             mPathfinding = true;
         }
-        else if(!mPathfinding)
+        else if (!mPathfinding)
             mCurrentWait -= Time.deltaTime;
 
         if (GetComponent<NavMeshAgent>().remainingDistance <= .025f && mPathfinding)
@@ -50,5 +57,18 @@ public class AIPatrolScript : MonoBehaviour {
             mPathfinding = false;
             mCurrentWait = mWaitTime;
         }
+
+        if (SeesPlayer())
+            GameObject.Find("Fade").GetComponent<FadeScript>().beginFade(.8f, 1, SceneManager.GetActiveScene().name);
+    }
+
+    private bool SeesPlayer()
+    {
+        Vector3 pos = GetComponent<Camera>().WorldToViewportPoint(mPlayer.transform.position);
+        if (!(pos.x > 0 && pos.x < 1 && pos.y > 0 && pos.y < 1 && pos.z > 0) || 
+            Vector3.Distance(mPlayer.transform.position, transform.position) > mSeeDistance)
+            return false;
+
+        return true;
     }
 }
---
+++ Senses Game/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/FadeScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Script that fades out and changes to a target scene, will also fade into target scene
+/// </summary>
+public class FadeScript : MonoBehaviour
+{
+
+    //Cite:https://www.youtube.com/watch?v=0HwZQt94uHQ
+    public Texture2D theBlackScreen;
+    public float fadeSpeed = .8f;
+    private static float speed;
+    private int drawDepth = -1000;
+    private float alpha = 1.0f;
+    private static int fadeDirection = -1;
+
+    private void Start()
+    {
+        speed = fadeSpeed;
+    }
+
+    void OnGUI()
+    {
+        alpha += fadeDirection * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.depth = drawDepth;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), theBlackScreen);
+    }
+
+    public float beginFade(float newFadingSpeed, int direction, string targetLevel)
+    {
+        if (newFadingSpeed != 0)
+            fadeSpeed = newFadingSpeed;
+
+        fadeDirection = direction;
+        if (targetLevel != "")
+            StartCoroutine(sceneSwitch(targetLevel));
+
+        return (speed);
+    }
+
+    void Awake()
+    {
+        beginFade(fadeSpeed, -1, "");
+    }
+
+    IEnumerator sceneSwitch(string targetLevel)
+    {
+        yield return new WaitForSeconds(.8f);
+        SceneManager.LoadScene(targetLevel);
+        yield break;
+    }
+}
