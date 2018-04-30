fb86cefa145bcff6d20a4926c16cb6da5f871449
Tyler <tyler.chermely@mymail.champlain.edu>
2018-04-30 19:36:37 -0400

    Added end game and new enemy

---
+++ Senses Game/Assets/Scripts/GameScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+    [SerializeField]
+    float mDistRequired = 3.0f;
+
+    [SerializeField]
+    Transform mGoalTransform;
+
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(mGoalTransform.position, transform.position) <= mDistRequired)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+	}
+}
---
+++ Senses Game/Assets/Scripts/TargetText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetText : MonoBehaviour {
+
+    [SerializeField]
+    GameObject playerObject;
+
+	// Update is called once per frame
+	void Update () {
+        transform.forward = -(playerObject.transform.position - transform.position).normalized;
+	}
+}
