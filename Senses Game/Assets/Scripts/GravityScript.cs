1cb5c69603e4162676cf2155526b912ab60b389e
Tyler <tyler.chermely@mymail.champlain.edu>
2018-04-30 23:35:07 -0400

    Added target spot

---
+++ Senses Game/Assets/Scripts/GravityScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityScript : MonoBehaviour
+{
+    [SerializeField]
+    private float mZOffset;
+
+    [SerializeField]
+    private float mChangeSpeed;
+
+    [SerializeField]
+    private float mMaxAlpha;
+
+    [SerializeField]
+    private int mPulseCount;
+
+    [SerializeField]
+    private float mPulseWidth;
+
+    [SerializeField]
+    private float mOffset;
+
+    private float mCurrentAlpha;
+    private List<float> mPulses;
+
+    /// <summary>
+    /// Set up pulse positions based on developer input
+    /// </summary>
+    void Start()
+    {
+        mCurrentAlpha = 0.0f;
+
+        mPulses = new List<float>();
+        float actualOffset = 0;
+        if (mOffset == 0)
+            actualOffset = 1;
+        else
+            actualOffset = mOffset;
+        for (int i = 0; i < mPulseCount * 2; i += 2)
+        {
+            mPulses.Add(GetComponent<MeshFilter>().mesh.bounds.extents.y - (i / 2 * actualOffset * ((GetComponent<MeshFilter>().mesh.bounds.size.y + mPulseWidth) / mPulseCount)) + mOffset);
+            mPulses.Add(GetComponent<MeshFilter>().mesh.bounds.extents.y - (i / 2 * actualOffset * ((GetComponent<MeshFilter>().mesh.bounds.size.y + mPulseWidth) / mPulseCount)) + mPulseWidth + mOffset);
+        }
+
+
+        GetComponent<MeshRenderer>().material.SetVector("center", GetComponent<MeshFilter>().mesh.bounds.center);
+    }
+
+    /// <summary>
+    /// Move the positions downward and set variables in shader. Wrap if necessary
+    /// </summary>
+    void Update()
+    {
+        for (int i = 0; i < mPulses.Count; i += 2)
+        {
+            mPulses[i] = mPulses[i] - mChangeSpeed * Time.deltaTime;
+            mPulses[i + 1] = mPulses[i + 1] - mChangeSpeed * Time.deltaTime;
+
+            if (mPulses[i + 1] < -GetComponent<MeshFilter>().mesh.bounds.extents.y - mOffset)
+            {
+                mPulses[i] = GetComponent<MeshFilter>().mesh.bounds.extents.y + mOffset;
+                mPulses[i + 1] = GetComponent<MeshFilter>().mesh.bounds.extents.y + mPulseWidth + mOffset;
+
+            }
+        }
+
+        Color old = GetComponent<MeshRenderer>().material.color;
+        GetComponent<MeshRenderer>().material.SetFloatArray("rectBounds", mPulses.ToArray());
+        GetComponent<MeshRenderer>().material.color = new Color(old.r, old.g, old.b, mCurrentAlpha);
+    }
+}
---
+++ Senses Game/Assets/Shaders/GravityShader.shader
@@ -0,0 +1,86 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Effect/Gravity"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1,1,1,1)
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+	}
+		SubShader
+	{
+		Tags{ "Queue" = "Transparent" "RenderType" = "Transparent" }
+		LOD 100
+
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+	{
+		CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+		// make fog work
+#pragma multi_compile_fog
+
+#include "UnityCG.cginc"
+
+		struct appdata
+	{
+		float4 vertex : POSITION;
+		float2 uv : TEXCOORD0;
+	};
+
+	struct v2f
+	{
+		float2 uv : TEXCOORD0;
+		float4 vertex : SV_POSITION;
+		float actualPos : TEXCOORD4;
+	};
+
+	float4 _Color;
+	sampler2D _MainTex;
+	float4 _MainTex_ST;
+	float3 center;
+	uniform float rectBounds[10];
+
+	v2f vert(appdata v)
+	{
+		v2f o;
+		o.vertex = UnityObjectToClipPos(v.vertex);
+		o.actualPos = v.vertex;
+		o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+		return o;
+	}
+
+	fixed4 frag(v2f i) : SV_Target
+	{
+		// sample the texture
+		fixed4 col = _Color;
+	col.a = .01f;
+	float distance = center.x - i.actualPos.x;
+	for (int j = 0; j < 10; j += 2)
+	{
+		float firstY = rectBounds[j];
+		float secondY = rectBounds[j + 1];
+
+		if (distance > firstY && distance < secondY)
+		{
+			float centerY = (firstY + secondY) / 2;
+			float range = secondY - firstY;
+			float result = ((centerY - distance) / range) / 2;
+			if (result < 1)
+			{
+				result = -result;
+			}
+
+			col.a = result;
+		}
+	}
+
+	return col;
+	}
+		ENDCG
+	}
+	}
+}
